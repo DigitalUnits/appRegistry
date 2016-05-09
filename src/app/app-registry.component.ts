ddf0f31959f03758d4667bfcfd169c2f33180b9f
Kent O. Johnson <kentoj@gmail.com>
2016-05-08 20:15:14 -0600

    Added components to main component

--- src/app/app-registry.component.ts
+++ src/app/app-registry.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { DgunAppRegistryComponent } from './dgun-app-registry/dgun-app-registry.component';
+import { DgunApplicationComponent } from './dgun-application/dgun-application.component';
+import { DgunDeveloperComponent } from './dgun-developer/dgun-developer.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-registry-app',
   templateUrl: 'app-registry.component.html',
   styleUrls: ['app-registry.component.css'],
-  directives: [ DgunAppRegistryComponent ]
+  directives: [ DgunAppRegistryComponent,
+                DgunApplicationComponent,
+                DgunDeveloperComponent]
 })
 export class AppRegistryAppComponent {
   title = 'app-registry works!';
